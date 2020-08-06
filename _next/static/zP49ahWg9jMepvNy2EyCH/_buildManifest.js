0ca494623861616c615dfa67d087a69c555511ab
Henry Corredor <hellwoodwen@gmail.com>
2020-08-06 18:05:55 +0800

    poems and paintings order changed

---
+++ _next/static/zP49ahWg9jMepvNy2EyCH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/collaborators":[a],"/conceptualperformance":[a],"/index":[a],"/oilpainting":[a],"/poems":[a]}}("static\u002Fchunks\u002F8a055d57c5eae54a838c7b7093517f3ff557631a.9bb4c4e1c2d14c1bd4db.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
