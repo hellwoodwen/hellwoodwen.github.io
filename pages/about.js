19f9ca14f159b90198b2ec984b7d469439560412
Henry Corredor <hellwoodwen@gmail.com>
2020-09-28 08:31:15 +0800

    editing about page, adding link to thesis and new text

--- pages/about.js
+++ pages/about.js
@@ -17,6 +17,25 @@ export default class extends React.Component {
                         <p>For enquiry please email</p>
                         <p>
                             <span>studio@hellwoodwen.com</span>
+                        </p>
+                        <p>
+                            <a href="file" className="filelink">How Interactive Design Language Could Manifest Uncertainty</a>
+                        </p>
+                        <p>
+                            <h2>Abstract</h2>
+                            <p>We are no longer living in a three-dimensional linear form. Discourses in cybernetics and
+                                quantum mechanics have fundamentally changed our way of social organization. The
+                                philosophy, theory, and technology have enabled globalization and interstellqr
+                                explorations. It is essential to rethink the definition of the ‘fourth dimension’ in the everaccelerating
+                                modernity that thrusts human beings into deeper space.</p>
+                            <p>The first part of this paper analyzes the development of timekeeping devices in both
+                                current day and ancient epoch. Chapter Two discusses the concept, making and
+                                references considered in the design of Present Revisit; how it reflects the contemporary
+                                theories in spacetime and the notions of present, ephemerality and eternity. Chapter
+                                Three takes a reflexive angle to inspect the diminishing of permanence in contemporary
+                                social modes of organization. The last passage emphasizes some underlying theories in
+                                regard to performance, observation, system and interaction.
+                            </p>
                             <br/>
                             <br/>
                             <br/>
@@ -26,17 +45,17 @@ export default class extends React.Component {
                             <br/>
                         </p>
                         <p>
-                            <a href="https://www.instagram.com/hellwoodwen/?hl=en" className="instagram">
+                            <a href="https://www.instagram.com/hellwoodwen/?hl=en" className="instagram icon">
                                 Instagram
                             </a>
-                            <a href="https://www.pinterest.com/hellwoodwen/" className="pinterest">Pinterest</a>
-                            <a href="https://www.linkedin.com/in/wen-wen-6a49a48a" className="linkedin">Linkedin</a>
-                            <a href="https://www.ucl.ac.uk/bartlett/architecture/present-revisit" className="ucl">UCL</a>
+                            <a href="https://www.pinterest.com/hellwoodwen/" className="pinterest icon">Pinterest</a>
+                            <a href="https://www.linkedin.com/in/wen-wen-6a49a48a" className="linkedin icon">Linkedin</a>
+                            <a href="https://www.ucl.ac.uk/bartlett/architecture/present-revisit" className="ucl icon">UCL</a>
                         </p>
                     </section>
                     <style jsx>
                         {`
-                        a {
+                        a.icon {
                           width: 50px;
                           height: 50px;
                           text-indent: -1000px;
