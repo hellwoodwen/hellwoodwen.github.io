8a8c52b1d4051a0f7a08a1d18e977e931afd9398
Henry Corredor <hellwoodwen@gmail.com>
2021-02-18 16:26:15 +0800

    fixed some css issues

---
+++ _next/static/FMCWW-d1iiak0dQDdubC9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/conceptualperformance":[a],"/eventsandexhibitions":[a],"/index":[a],"/oilpainting":[a],"/poems":[a]}}("static\u002Fchunks\u002F5877cfea2dbb8f5253a1086be48c7b0742307085.1cafebb183beb2bd14c2.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
